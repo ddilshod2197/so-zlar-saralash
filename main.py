2e50db967de25f2f2cf9df2f3660a0b9a91d48a0
Dilshod <ddilshod2197@gmail.com>
2026-05-02 16:57:12 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Iltimos, to'g'ri son kiritng."
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < n:
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence
+
+n = int(input("Dastlabki N a'zolar sonini kiriting: "))
+print(fibonacci(n))
